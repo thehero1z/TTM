6c3bae0ea526d3a839439d0b2579988b06b402c3
Appiah Boateng <info.ystylist@gmail.com>
2023-03-05 12:13:24 -0500

    hamburger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       screens: {
         'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
         'tallscreen': {'raw': '(min-aspect-ratio: 13/20)'},
+      },
+      keyframes: {
+        'open-menu': { 
+          '0%': {'transform: 'scaleY(0)'},
+          '80%': {'transform: 'scaleY(1.2)'},
+          '100%': {'transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
